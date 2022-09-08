392f984a057a2666d6ef4233e96b8bf5c1e7ed71
Sanjay Gautam <sanjaykumargautam535@gmail.com>
2022-09-08 11:17:04 +0530

    updated user deatils for each client

--- public/script.js
+++ public/script.js
@@ -11,23 +11,21 @@ let startX;
 let startY;
 
 const draw = (e) => {
+  e.preventDefault();
   if (!isPainting) {
     return;
   }
   ctx.lineWidth = lineWidth;
   ctx.lineCap = 'round';
+  ctx.lineJoin = 'round';
   // console.log('e.clientX= ', e.clientX, ' e.clientY=', e.clientY);
   const bounds = canvas.getBoundingClientRect();
   // get the mouse coordinates, subtract the canvas top left and any scrolling
 
   let mouseX = e.pageX - bounds.left - scrollX;
   let mouseY = e.pageY - bounds.top - scrollY;
-  // first normalize the mouse coordinates from 0 to 1 (0,0) top left
-  // off canvas and (1,1) bottom right by dividing by the bounds width and height
   mouseX /= bounds.width;
   mouseY /= bounds.height;
-
-  // then scale to canvas coordinates by multiplying the normalized coords with the canvas resolution
 
   mouseX *= canvas.width;
   mouseY *= canvas.height;
@@ -44,6 +42,7 @@ const draw = (e) => {
   ctx.stroke();
 };
 
+// mouse events
 canvas.addEventListener('mousedown', (e) => {
   mouseDown(e);
 });
@@ -57,20 +56,73 @@ canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseleave', () => {
   mouseLeave();
 });
+// end mouse event
 
 // mobile screen
 canvas.addEventListener('pointerdown', (e) => {
   mouseDown(e);
 });
+
 canvas.addEventListener('pointerup', () => {
   mouseUp();
 });
+
 canvas.addEventListener('pointermove', (e) => {
   draw(e);
 });
+
 canvas.addEventListener('pointerleave', () => {
   mouseLeave();
 });
+
+canvas.addEventListener('touchmove', (e) => {
+  touchMove(e);
+});
+canvas.addEventListener('touchstart', (e) => {
+  touchStart(e);
+});
+
+function touchMove(e) {
+  e.preventDefault();
+  if (!isPainting) {
+    return;
+  }
+  ctx.lineWidth = lineWidth;
+  ctx.lineCap = 'round';
+  ctx.lineJoin = 'round';
+  // console.log('e.clientX= ', e.clientX, ' e.clientY=', e.clientY);
+  const bounds = canvas.getBoundingClientRect();
+  // get the mouse coordinates, subtract the canvas top left and any scrolling
+
+  let mouseX = e.touches[0].clientX - bounds.left - scrollX;
+  let mouseY = e.touches[0].clientY - bounds.top - scrollY;
+  mouseX /= bounds.width;
+  mouseY /= bounds.height;
+
+  mouseX *= canvas.width;
+  mouseY *= canvas.height;
+
+  let data = {
+    lineWidth: lineWidth,
+    rang: ctx.strokeStyle,
+    x: mouseX,
+    y: mouseY,
+  };
+  socket.emit('draw', data);
+  // ctx.lineTo(e.clientX - canvasOffsetX, e.clientY - canvasOffsetY);
+  ctx.lineTo(mouseX, mouseY);
+  ctx.stroke();
+}
+
+function touchStart(e) {
+  isPainting = true;
+  startX = e.touches[0].clientX;
+  startY = e.touches[0].clientY;
+  ctx.beginPath();
+  socket.emit('mouse', { sx: startX, sy: startY });
+}
+
+// end mobile screens event
 
 function mouseLeave() {
   isPainting = false;
@@ -100,6 +152,7 @@ socket.on('draw', (data) => {
   ctx.lineWidth = data.lineWidth;
   ctx.strokeStyle = data.rang;
   ctx.lineCap = 'round';
+  ctx.lineJoin = 'round';
   ctx.lineTo(data.x, data.y);
   ctx.stroke();
 });
@@ -150,6 +203,7 @@ const strokeWidthColor = document.querySelectorAll('.mm');
 document.getElementById('moon').onclick = (e) => {
   console.log(e.target.id);
   if (e.target.id === 'moon') {
+    $('#dropdownMenuLink').css('color', '#343a40');
     strokeWidthColor.forEach((div) => (div.style.backgroundColor = '#343a40'));
     e.target.id = 'light';
     strokeWidth.style.color = '#343a40';
@@ -158,6 +212,7 @@ document.getElementById('moon').onclick = (e) => {
     ctx.strokeStyle = '#fff';
     canvas.style.backgroundColor = '#343a40';
   } else {
+    $('#dropdownMenuLink').css('color', '#fff');
     strokeWidthColor.forEach((div) => (div.style.backgroundColor = '#fff'));
     e.target.id = 'moon';
     e.target.src = './img/free_icon_1.svg';
@@ -167,3 +222,42 @@ document.getElementById('moon').onclick = (e) => {
     canvas.style.backgroundColor = '#fff';
   }
 };
+
+$(document).ready(function () {
+  $('#myModal').modal('show');
+  $('#submit').click(function (e) {
+    e.preventDefault();
+    const name = $('#name').val();
+    if (name.length === 0) {
+      $('#myModal').modal('show');
+    } else {
+      socket.emit('user-details', { userName: name, socketId: socket.id });
+      $('#myModal').modal('hide');
+    }
+  });
+});
+
+$(document).click(function () {
+  $('#myModal').on('hidden.bs.modal', function () {
+    const name = $('#name').val();
+    if (name.length > 0) {
+      $('#myModal').modal('hide');
+    } else {
+      $('#myModal').modal('show');
+    }
+  });
+});
+
+// update users in front-end
+console.log($('#dropdown-menu'));
+socket.on('user-details', (users) => {
+  $('#dropdown-menu').html(`
+   ${users.map((user) => {
+     if (user.socketId === socket.id) {
+       return `<p class="dropdown-item" style="background-color:#ccc; color:#343a40" href="#">${user.userName}</p>`;
+     }
+     return `<p class="dropdown-item" href="#">${user.userName}</p>`;
+   })}
+  `);
+  console.log(users);
+});
--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const io = socketio(server); //it enable socket for server as well as for client
 
 app.use('/', express.static(__dirname + '/public'));
 
+let users = [];
+
 io.on('connection', (socket) => {
   console.log('connection= ', socket.id);
   socket.on('mouse', (data) => {
@@ -20,6 +22,19 @@ io.on('connection', (socket) => {
     // console.log('data= ', data);
     // io.to(data.id).emit('draw', { data }); to same socket id
     socket.broadcast.emit('draw', data);
+  });
+
+  socket.on('user-details', (data) => {
+    users.push(data);
+    io.sockets.emit('user-details', users);
+    // socket.broadcast.emit('user-details', users);
+    console.log('users= ', users);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    users = users.filter((user) => user.socketId !== socket.id);
+    console.log('users= ', users);
   });
 });
 
